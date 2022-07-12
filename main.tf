c125fc09772a8d378f78eae6593999a90e1bb1b1
Manthan Sharma <manthan.sharma@infostretch.com>
2022-07-12 14:01:39 +0530

    first terraform files

---
+++ main.tf
@@ -0,0 +1,6 @@
+resource "github_repository" "example1" {
+  name        = "example"
+  description = "My codebase"
+
+  visibility = "public"
+}
---
+++ provider.tf
@@ -0,0 +1,4 @@
+provider "github" {
+  token = "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"
+  organization = "sharma-manthan"
+}
